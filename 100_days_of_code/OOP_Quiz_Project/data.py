63335a119005dfe36f78818d51ab37fbafbd0323
NaokiKircia <32564958+NaokiKircia@users.noreply.github.com>
2022-02-11 21:21:36 +0100

    finished lesson project

--- 100_days_of_code/OOP_Quiz_Project/data.py
+++ 100_days_of_code/OOP_Quiz_Project/data.py
@@ -1,16 +1,117 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
-             "you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
-     "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "Greenland is almost as big as Africa.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Music", "type": "boolean",
+        "difficulty": "easy",
+        "question": "The music video to The Buggle&#039;s &quot;Video Killed the Radio Star&quot; was the first music video to broadcast on MTV.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge", "type": "boolean",
+        "difficulty": "easy",
+        "question": "Gumbo is a stew that originated in Louisiana.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "San Marino is the only country completely surrounded by another country.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film", "type": "boolean",
+        "difficulty": "easy",
+        "question": "The 2010 film &quot;The Social Network&quot; is a biographical drama film about MySpace founder Tom Anderson.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Gadgets", "type": "boolean",
+        "difficulty": "easy",
+        "question": "The communication protocol NFC stands for Near-Field Control.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science & Nature", "type": "boolean",
+        "difficulty": "easy", "question": "Salt is 100% composed of Sodium.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean",
+        "difficulty": "easy",
+        "question": "Solid Snake is actually a clone from the DNA of Big Boss in the Metal Gear Solid series&#039; history.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean",
+        "difficulty": "easy",
+        "question": "Deus Ex (2000) does not feature the World Trade Center because it was destroyed by terrorist attacks according to the game&#039;s plot.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History", "type": "boolean", "difficulty": "easy",
+        "question": "Former United States Presidents John Adams and Thomas Jefferson died within hours of each other.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Team Fortress 2, being disguised as a scout or medic results in a speed boost.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean",
+        "difficulty": "easy",
+        "question": "In the game &quot;Subnautica&quot;, a &quot;Spadefish&quot; will attack you.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean",
+        "difficulty": "easy",
+        "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Pok&eacute;mon Sun and Moon, a male Salandit can evolve to a Salazzle.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Television", "type": "boolean",
+        "difficulty": "easy",
+        "question": "In &quot;Star Trek&quot;, Klingons are aliens.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Film", "type": "boolean",
+        "difficulty": "easy",
+        "question": "Actor Tommy Chong served prison time.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Vehicles", "type": "boolean", "difficulty": "easy",
+        "question": "BMW M GmbH is a subsidiary of BMW AG that focuses on car performance.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Mathematics", "type": "boolean",
+        "difficulty": "easy",
+        "question": "An isosceles triangle has two sides of equal length as opposed to three.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+
+        "category": "Entertainment: Music", "type": "boolean",
+        "difficulty": "easy",
+        "question": "John Williams composed the music for &quot;Star Wars&quot;.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports", "type": "boolean", "difficulty": "easy",
+        "question": "There are a total of 20 races in Formula One 2016 season.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    }
 ]
--- 100_days_of_code/OOP_Quiz_Project/main.py
+++ 100_days_of_code/OOP_Quiz_Project/main.py
@@ -4,12 +4,15 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-quizz = QuizBrain(question_bank)
+quiz = QuizBrain(question_bank)
 
-while quizz.still_has_questions():
-    quizz.next_question()
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
