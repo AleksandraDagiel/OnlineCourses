177cfd65b17c86fe26688d3f884d95ea7101cd35
Patryk Dagiel <patryk.dagiel@gmail.com>
2022-02-14 21:33:58 +0100

    OOP: Turtle canvas Paining with dots 10x10

--- 100_days_of_code/OOP_Timmy_the_Turtle/dot_painting_project.py
+++ 100_days_of_code/OOP_Timmy_the_Turtle/dot_painting_project.py
@@ -6,27 +6,37 @@ t = turtle.Turtle()
 turtle.colormode(255)
 
 
-def get_colors_from_picture(picture, num_of_colors):
-    colors_palette = colorgram.extract(picture, num_of_colors)
-    color_list = []
-    for color in colors_palette:
-        rgb = (colors_palette[color]).rgb
-        color_list.append(rgb)
-    return color_list
+# def get_colors_from_picture(picture, num_of_colors):
+#     colors_palette = colorgram.extract(picture, num_of_colors)
+#     color_list = []
+#     for color in colors_palette:
+#         r = color.rgb.r
+#         g = color.rgb.g
+#         b = color.rgb.b
+#         color_list.append((r, g, b))
+#     return color_list
+#
+#
+# rgb_list = get_colors_from_picture('dot_painting_color_sample.jpg', 30)
 
+color_palette = [
+    (132, 166, 205), (221, 148, 106), (32, 42, 61), (199, 135, 148), (166, 58, 48), (141, 184, 162), (39, 105, 157),
+    (237, 212, 90), (150, 59, 66), (216, 82, 71), (168, 29, 33), (235, 165, 157), (51, 111, 90), (35, 61, 55),
+    (156, 33, 31), (17, 97, 71), (52, 44, 49), (230, 161, 166), (170, 188, 221), (57, 51, 48), (184, 103, 113),
+    (32, 60, 109), (105, 126, 159), (175, 200, 188), (34, 151, 210), (65, 66, 56)
+]
 
-color_palette_list = get_colors_from_picture('dot_painting_color_sample.jpg', 30)
-max_num_of_sides = 11
-num_of_sides = 3
-t.speed('fastest')
+t.speed(8)
+t.penup()
+t.setposition(-250, -250)
+t.hideturtle()
 
-# draw shapes from triangle to max_num_of_sides in different random colors
-while not max_num_of_sides == num_of_sides:
-    t.color(random.choice(color_palette_list))
-    for i in range(num_of_sides):
-        t.right(360 / num_of_sides)
-        t.forward(100)
-    num_of_sides += 1
+for _ in range(10):
+    position = t.position()
+    for _ in range(10):
+        t.dot(20, (random.choice(color_palette)))
+        t.forward(50)
+    t.goto(x=position[0], y=position[1]+50)
 
 
 screen = turtle.Screen()
