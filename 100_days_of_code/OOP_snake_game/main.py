f51f5cb58f220d3ce08aee73c0cfd9ed045a979c
NaokiKircia <32564958+NaokiKircia@users.noreply.github.com>
2022-02-21 16:31:37 +0100

    Highest score is written in the file

--- 100_days_of_code/OOP_snake_game/main.py
+++ 100_days_of_code/OOP_snake_game/main.py
@@ -1,7 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
-from score_board import ScoreBoard
+from score_board import Scoreboard
 import time
 
 screen = Screen()
@@ -12,10 +12,10 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-score_board = ScoreBoard()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up,"Up")
+screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
@@ -24,35 +24,27 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    # Detect collision with food.
-    if snake.head.distance(food) < 15:
-        snake.extend()
-        food.refresh()
-        score_board.point_up()
-
-    # Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score_board.game_over()
-
-    # Detect collision with the tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score_board.game_over()
-
     snake.move()
 
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.reset()
 
-
-
-
-
-
-
-
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.reset()
 
 
 screen.exitonclick()
+
--- 100_days_of_code/OOP_snake_game/score_board.py
+++ 100_days_of_code/OOP_snake_game/score_board.py
@@ -1,30 +1,36 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
-FONT = ("Courier", 18, "bold")
-ALIGN = 'center'
 
-
-class ScoreBoard(Turtle):
+class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.hideturtle()
-        self.penup()
+        self.score = 0
+        with open("data.txt", mode="r") as data:
+            self.high_score = int(data.read())
         self.color("white")
+        self.penup()
         self.goto(0, 270)
-        self.score_points = 0
-        self.score()
+        self.hideturtle()
+        self.update_scoreboard()
 
-    def score(self):
-        self.write(arg=f"Score: {self.score_points}", font=FONT, align=ALIGN)
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", font=FONT, align=ALIGN)
-
-    def point_up(self):
+    def update_scoreboard(self):
         self.clear()
-        self.score_points += 1
-        self.score()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
 
 
