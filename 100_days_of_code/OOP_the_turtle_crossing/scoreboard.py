93550a16aeeba497682dcd57eebf59be7aa7b00a
NaokiKircia <32564958+NaokiKircia@users.noreply.github.com>
2022-02-21 16:32:01 +0100

    issue solved

--- 100_days_of_code/OOP_the_turtle_crossing/scoreboard.py
+++ 100_days_of_code/OOP_the_turtle_crossing/scoreboard.py
@@ -6,8 +6,8 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
 
     def __init__(self):
-        super(Scoreboard, self).__init__()
-        self.level =1
+        super().__init__()
+        self.level = 1
         self.hideturtle()
         self.penup()
         self.goto(-280, 250)
