b483ba391261ffbff2c62f6d9842807920d48d26
NaokiKircia <naoki.kircia@gmail.com>
2022-03-01 20:13:17 +0100

    layout setup

--- 100_days_of_code/Tkinter_password_manager/main.py
+++ 100_days_of_code/Tkinter_password_manager/main.py
@@ -8,28 +8,34 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
+# Canvas
 lock_img = PhotoImage(file="logo.png")
 canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=lock_img)
-canvas.grid(column=1, row=0)
+canvas.grid(row=0, column=1)
 
-website_label = Label(text="Website")
-website_label.grid(column=0, row=1)
-email_label = Label(text="Email")
-email_label.grid(column=0, row=2)
-password_label = Label(text="Password")
-password_label.grid(column=0, row=3)
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
 
-
+# Buttons
 generate_password_button = Button(text="Generate Password")
-generate_password_button.grid(column=2, row=3)
-add_button = Button(text="Add")
-add_button.grid(column=1, row=4, columnspan=2)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2, sticky=EW)
 
-website_entry = Entry()
-website_entry.grid(column=1, row=3, columnspan=2)
-email_entry = Entry()
-password_entry = Entry()
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2, sticky=EW)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2, sticky=EW)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1, sticky=EW)
+
 
 
 
