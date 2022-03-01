a9d87d9c2a82d98a930b22d27333f4b379a13ec3
NaokiKircia <naoki.kircia@gmail.com>
2022-03-01 21:41:15 +0100

    working on "save" button

--- 100_days_of_code/Tkinter_password_manager/main.py
+++ 100_days_of_code/Tkinter_password_manager/main.py
@@ -3,6 +3,17 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def save():
+    password_field = password_entry.get()
+    email_field = email_entry.get()
+    website_field = website_entry.get()
+    with open("data.txt", "a") as data:
+        data.write(f"{website_field} | {email_field} | {password_field}\n")
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -25,14 +36,16 @@ password_label.grid(row=3, column=0)
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky=EW)
 
 # Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2, sticky=EW)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, sticky=EW)
+email_entry.insert(0, "ag@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky=EW)
 
