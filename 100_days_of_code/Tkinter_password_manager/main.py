5067ef7b37cf6253061ae9ab8fdf26415025e5b6
NaokiKircia <32564958+NaokiKircia@users.noreply.github.com>
2022-03-02 10:40:32 +0100

    genwerate random password when clicking button

--- 100_days_of_code/Tkinter_password_manager/main.py
+++ 100_days_of_code/Tkinter_password_manager/main.py
@@ -4,36 +4,20 @@ import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    password_list = []
+    password_list += [random.choice(letters) for letter in range(random.randint(8, 10))]
+    password_list += [random.choice(symbols) for symbol in range(random.randint(2, 4))]
+    password_list += [random.choice(numbers) for number in range(random.randint(2, 4))]
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    random.shuffle(password_list)
+    password = "".join(password_list)
 
-password_list = []
-
-password_list += [str(char) for char in range(nr_letters)]
-# for char in range(nr_letters):
-#   password_list.append(random.choice(letters))
-password_list += [str(char) for char in range(nr_symbols)]
-
-# for char in range(nr_symbols):
-#   password_list += random.choice(symbols)
-password_list += [str(char) for char in range(nr_numbers)]
-
-# for char in range(nr_numbers):
-#   password_list += random.choice(numbers)
-
-random.shuffle(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
+    password_entry.insert(0, password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -75,7 +59,7 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky=EW)
