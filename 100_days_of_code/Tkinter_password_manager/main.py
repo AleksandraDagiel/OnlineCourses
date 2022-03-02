aa63d0179fbf042e0ba7b686c8d241e15b71d66f
NaokiKircia <32564958+NaokiKircia@users.noreply.github.com>
2022-03-02 10:42:28 +0100

    copy password to clipboard

--- 100_days_of_code/Tkinter_password_manager/main.py
+++ 100_days_of_code/Tkinter_password_manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
@@ -18,6 +19,7 @@ def generate_password():
     password = "".join(password_list)
 
     password_entry.insert(0, password)
+    pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
