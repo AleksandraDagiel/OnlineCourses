9cdac59c5372489e80199e99d826dc032a81348e
NaokiKircia <32564958+NaokiKircia@users.noreply.github.com>
2022-03-03 17:43:37 +0100

    Setup

---
+++ 100_days_of_code/flash_card_project/main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+card_back = PhotoImage("./images/card_back.png")
+card_front = PhotoImage("./images/card_front.png")
+right = PhotoImage("./images/right.png")
+wrong = PhotoImage("./images/wrong.png")
