7e1b7fa2732f17145f04f4a3ad8f741e10a4b9ca
NaokiKircia <Nezumi19$>
2022-03-05 22:17:42 +0100

    project finished with 2 solutions (mine & course solution)

--- 100_days_of_code/smtp_birthday_wisher_project/main.py
+++ 100_days_of_code/smtp_birthday_wisher_project/main.py
@@ -1,22 +1,56 @@
-##################### Hard Starting Project ######################
+import pandas
+import random
+import smtplib
+import datetime
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
-
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+my_email = "testing.acc.udemy@gmail.com"
+password = "yololo123"
 
 
+# My way
+# today = datetime.datetime.now()
+# today_day = today.day
+# today_month = today.month
+# data = pandas.read_csv("birthdays.csv")
+# birthday_dict = data.to_dict("records")
+#
+#
+# for person in birthday_dict:
+#     bday_day = person["day"]
+#     bday_month = person["month"]
+#
+#     if today_day == bday_day and today_month == bday_month:
+#         random_letter = random.randint(1, 3)
+#
+#         with open(f"./letter_templates/letter_{random_letter}.txt", "r") as letter:
+#             letter = letter.read()
+#             personalised_letter = letter.replace("[NAME]", person["name"])
+#
+#         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+#             connection.starttls()
+#             connection.login(user=my_email, password=password)
+#             connection.sendmail(from_addr=my_email, to_addrs=person["email"],
+#                                 msg=f"Subject: Happy Birthday \n\n {personalised_letter}")
+#
 
+# Course solution
+
+today = datetime.datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[Name]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=birthday_person["email"],
+                            msg=f"Subject: Happy Birthday \n\n {contents}")
