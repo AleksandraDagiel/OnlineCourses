cd2b06ea01f43734ccc8e8237a9088845abad3d6
NaokiKircia <32564958+NaokiKircia@users.noreply.github.com>
2022-03-14 18:52:01 +0100

    Progress saving

--- 100_days_of_code/API_workout_tracker_project/main.py
+++ 100_days_of_code/API_workout_tracker_project/main.py
@@ -2,3 +2,22 @@ import requests
 
 API_KEY = "1cd8219f2de0179a9833f0ce1e642bdf"
 APP_ID = "a81deb91"
+
+HEADERS = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+user_input = input("Tell me which exercise you did: ")
+
+NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+parameters = {
+    "query": user_input,
+    "gender": "female",
+    "weight_kg": 56,
+    "height_cm": 163,
+    "age": 29,
+}
+
+response = requests.post(url=NUTRITIONIX_ENDPOINT, json=parameters, headers=HEADERS)
+result = response.json()
+print(response.text, result)
