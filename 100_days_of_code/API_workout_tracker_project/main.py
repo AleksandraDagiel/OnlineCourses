20328c23ab5e13ec518ee683234663eedebcfb62
NaokiKircia <32564958+NaokiKircia@users.noreply.github.com>
2022-03-15 11:21:15 +0100

    complete project

--- 100_days_of_code/API_workout_tracker_project/main.py
+++ 100_days_of_code/API_workout_tracker_project/main.py
@@ -1,13 +1,21 @@
 import requests
+import datetime as dt
+import os
 
-API_KEY = "1cd8219f2de0179a9833f0ce1e642bdf"
-APP_ID = "a81deb91"
-SHEETTY_ENDPOINT = "https://api.sheety.co/2bd1c51b84d0ec2c017e0e6ba8cff906/myWorkouts/workouts"
+API_ID = os.environ.get("API_ID")
+API_KEY = os.environ.get("API_KEY")
+SHEETY_ENDPOINT = os.environ.get("SHEETY_ENDPOINT")
+AUTH_BEARER = os.environ.get("AUTH_BEARER")
 
 HEADERS = {
-    "x-app-id": APP_ID,
+    "x-app-id": API_ID,
     "x-app-key": API_KEY,
 }
+
+TOKEN = {
+    "Authorization": f"Bearer {AUTH_BEARER}",
+}
+
 user_input = input("Tell me which exercise you did: ")
 
 NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
@@ -21,16 +29,19 @@ parameters = {
 
 response = requests.post(url=NUTRITIONIX_ENDPOINT, json=parameters, headers=HEADERS)
 result = response.json()
-print(response.text, result)
 
-# Add row
-result["exercises"]code
-exercise_params = {
-    "workout": {
-        "Date": today,
-        "Time": time_spent,
-        "Exercise": exercise,
-        "Duration": duration,
-        "Calories": calories,
+today = dt.datetime.now().strftime("%d/%m/%Y")
+time = dt.datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    exercise_params = {
+        "workout": {
+            "date": today,
+            "time": time,
+            "exercise": exercise["user_input"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
     }
-}
+    response = requests.post(url=SHEETY_ENDPOINT, json=exercise_params, headers=TOKEN)
+    print(response.text)
