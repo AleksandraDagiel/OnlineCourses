f10c4f60733bb2d7e48af89379ed3c60817885a8
NaokiKircia <Nezumi19$>
2022-03-15 08:35:19 +0100

    Progress saving

--- 100_days_of_code/API_workout_tracker_project/main.py
+++ 100_days_of_code/API_workout_tracker_project/main.py
@@ -2,6 +2,7 @@ import requests
 
 API_KEY = "1cd8219f2de0179a9833f0ce1e642bdf"
 APP_ID = "a81deb91"
+SHEETTY_ENDPOINT = "https://api.sheety.co/2bd1c51b84d0ec2c017e0e6ba8cff906/myWorkouts/workouts"
 
 HEADERS = {
     "x-app-id": APP_ID,
@@ -21,3 +22,15 @@ parameters = {
 response = requests.post(url=NUTRITIONIX_ENDPOINT, json=parameters, headers=HEADERS)
 result = response.json()
 print(response.text, result)
+
+# Add row
+result["exercises"]code
+exercise_params = {
+    "workout": {
+        "Date": today,
+        "Time": time_spent,
+        "Exercise": exercise,
+        "Duration": duration,
+        "Calories": calories,
+    }
+}
