6cee3783b06294822e8cf7d03e6c113fe2509ae0
AleksandraDagiel <naoki.kircia@gmail.com>
2023-11-06 18:02:26 +0100

    submit move method

--- Other/TicTacToe_Game/board.py
+++ Other/TicTacToe_Game/board.py
@@ -25,4 +25,12 @@ class Board:
     def print_board_with_positions(self):
         print("| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |")
 
+    def submit_move(self, player, move):
+        row = move.get_row()
+        col = move.get_column()
+        value = self.game_board[row][col]
 
+        if value == Board.EMPTY_CELL:
+            self.game_board[row][col] = player.marker
+        else:
+            print("This position is already taken. please enter another one.")
