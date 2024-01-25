c4fc22b0fb8b789c5594fb22f6fe0d0996f7e968
AleksandraDagiel <naoki.kircia@gmail.com>
2024-01-25 22:10:02 +0100

    Untrack files in .gitignore

--- Playwright_From_Zero_To_Hero/pw-practice-app/tests/firstTest.spec.ts
+++ Playwright_From_Zero_To_Hero/pw-practice-app/tests/firstTest.spec.ts
@@ -1,5 +1,47 @@
 import {test} from '@playwright/test'
 
-test('the first test', async ({page}) => {
+test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
+})
+
+test.describe('suite1', () => {
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Forms').click()
+    })
+
+    test('the first test', async ({page}) => {
+        await page.getByText('Form Layouts').click()
+    })
+    
+    test('second test', async ({page}) => {
+        await page.getByText('Datepicker').click()
+    })
+})
+
+test.describe('suite2', () => {
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Modal & Overlays').click()
+    })
+
+    test('third test', async ({page}) => {
+        await page.getByText('Dialog').click()
+    })
+    
+    test('fourth test', async ({page}) => {
+        await page.getByText('Window').click()
+    })
+})
+
+test.describe.skip('suite3', () => {
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Charts').click()
+    })
+
+    test('fifth test', async ({page}) => {
+        await page.getByText('Echarts').click()
+    })
+})
+
+test.afterEach(async ({page}) => {
+    page.close()
 })
