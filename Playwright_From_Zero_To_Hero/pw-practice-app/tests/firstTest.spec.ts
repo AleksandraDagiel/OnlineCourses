4cc351cd34ff4de942fbdb5a0bb371102687adb1
AleksandraDagiel <naoki.kircia@gmail.com>
2024-01-27 16:26:05 +0100

    reusing the locators

--- Playwright_From_Zero_To_Hero/pw-practice-app/tests/firstTest.spec.ts
+++ Playwright_From_Zero_To_Hero/pw-practice-app/tests/firstTest.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test'
+import {expect, test} from '@playwright/test'
 
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
@@ -123,6 +123,20 @@ test.describe("Geting locator that are visible by user", () => {
         //not recommended by xpath:
         await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Email"}).click()
     })
+
+    test('Reusing the locators', async({page}) => {
+        const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+        const emailField = basicForm.getByRole('textbox', {name: "Email"})
+        
+        await emailField.fill('test123@test.com')
+        await basicForm.getByRole('textbox', {name: "Password"}).fill('Welcome123')
+        await basicForm.locator('nb-checkbox').click()
+        await basicForm.getByRole('button').click()
+
+        await expect(emailField).toHaveValue('test123@test.com')
+    })
+
+
 })
 
 
