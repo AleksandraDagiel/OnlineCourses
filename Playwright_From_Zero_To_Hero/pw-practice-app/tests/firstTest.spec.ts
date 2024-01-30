6c12f4f3bd7396749c3e014138295eac0b1080cf
AleksandraDagiel <naoki.kircia@gmail.com>
2024-01-30 17:59:22 +0100

    extracting values

--- Playwright_From_Zero_To_Hero/pw-practice-app/tests/firstTest.spec.ts
+++ Playwright_From_Zero_To_Hero/pw-practice-app/tests/firstTest.spec.ts
@@ -135,8 +135,38 @@ test.describe("Geting locator that are visible by user", () => {
 
         await expect(emailField).toHaveValue('test123@test.com')
     })
-
-
 })
 
+test.describe("Extracting values", () => {
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Forms').click()
+        await page.getByText('Form Layouts').click()
+    })
+    
+    test('single text value', async({page}) => {
+        const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+        const buttonText = await basicForm.locator('button').textContent()
+        expect(buttonText).toEqual('Submit')
+    })
 
+    test('all text values', async({page}) => {
+        const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+        const allRadioButtonsLabel = await page.locator('nb-radio').allTextContents()
+        expect(allRadioButtonsLabel).toContain("Option 1")
+    })
+
+    test('input value', async({page}) => {
+        const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+        const emailField = basicForm.getByRole('textbox', {name: "Email"})
+        await emailField.fill('test@test.com')
+        const emailValue = await emailField.inputValue()
+        expect(emailValue).toEqual('test@test.com')
+    })
+
+    test('attribute value', async({page}) => {
+        const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+        const emailField = basicForm.getByRole('textbox', {name: "Email"})
+        const placeholderValue = await emailField.getAttribute('placeholder')
+        expect(placeholderValue).toEqual('Email')
+    })
+})
