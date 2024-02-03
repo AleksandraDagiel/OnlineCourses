eaa00bb6808f1044486239338d730b98a15968a1
AleksandraDagiel <naoki.kircia@gmail.com>
2024-02-03 17:29:44 +0100

    assertions

--- Playwright_From_Zero_To_Hero/pw-practice-app/tests/firstTest.spec.ts
+++ Playwright_From_Zero_To_Hero/pw-practice-app/tests/firstTest.spec.ts
@@ -170,3 +170,26 @@ test.describe("Extracting values", () => {
         expect(placeholderValue).toEqual('Email')
     })
 })
+
+test.beforeEach(async ({page}) => {
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('assertions', async({page}) => {
+    const basicFormButton = page.locator('nb-card').filter({hasText: "Basic form"}).locator("button")
+
+    //General assertions
+    const value = 5
+    expect(value).toEqual(5)
+
+    const text = await basicFormButton.textContent()
+    expect(text).toEqual("Submit")
+
+    //Locator assertion
+    await expect(basicFormButton).toHaveText("Submit")
+
+    //Soft Assertion
+    await expect.soft(basicFormButton).toHaveText("Submit5")
+    await basicFormButton.click()
+})
